9324b46a23a7deece969cfba044c0f0d287bd150
Nolan Martin <nmartindevelopment@gmail.com>
2024-11-28 09:12:57 -0500

    Create login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:devil_scout/pages/login.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -10,11 +12,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
