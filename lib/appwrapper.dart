6e195ed1251c4e5c06b9048cfcd72dcf1a0c36f0
Nolan Martin <nmartindevelopment@gmail.com>
2025-01-03 13:31:08 -0500

    Add shadow to navbar

--- lib/appwrapper.dart
+++ lib/appwrapper.dart
@@ -26,35 +26,46 @@ class _AppWrapperState extends State<AppWrapper> {
           ManageHomePage(),
         ],
       ),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _pageIndex,
-        onDestinationSelected: (int index) {
-          setState(() {
-            _pageIndex = index;
-          });
-        },
-        destinations: <NavigationDestination>[
-          NavigationDestination(
-            icon: Icon(Icons.home_outlined),
-            selectedIcon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.assignment_outlined),
-            selectedIcon: Icon(Icons.assignment),
-            label: 'Scout',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.analytics_outlined),
-            selectedIcon: Icon(Icons.analytics),
-            label: 'Analyze',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.settings_outlined),
-            selectedIcon: Icon(Icons.settings),
-            label: 'Manage',
-          ),
-        ],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Theme.of(context).colorScheme.onSurface.withOpacity(0.1),
+              blurRadius: 6.0,
+              offset: const Offset(0.0, -3.0),
+            ),
+          ],
+        ),
+        child: NavigationBar(
+          selectedIndex: _pageIndex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              _pageIndex = index;
+            });
+          },
+          destinations: <NavigationDestination>[
+            NavigationDestination(
+              icon: Icon(Icons.home_outlined),
+              selectedIcon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.assignment_outlined),
+              selectedIcon: Icon(Icons.assignment),
+              label: 'Scout',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.analytics_outlined),
+              selectedIcon: Icon(Icons.analytics),
+              label: 'Analyze',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.settings_outlined),
+              selectedIcon: Icon(Icons.settings),
+              label: 'Manage',
+            ),
+          ],
+        ),
       ),
     );
   }
